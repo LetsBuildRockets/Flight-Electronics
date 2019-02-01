e5d42de4e57c6142967c9e5492f48f3d03db1ee4
Eric Sims <EricSimsM@gmail.com>
2019-02-01 00:03:31 -0500

    Alitmeter request conversion, and OPT, still need to calculate actual press/temp from these

--- Flight_Electronics_CPU/Altimeter.cpp
+++ Flight_Electronics_CPU/Altimeter.cpp
@@ -28,29 +28,99 @@ void Altimeter::init()
 		char buffer[5];
 		itoa(partNumber & 0b11111, buffer, 2);
 		Telemetry::printf(MSG_INFO, "Altimeter detected: ICP-%s\n", buffer);
-		send16BitWord(0x805D); // soft reset
 	}
 	else
 	{
 		Telemetry::printf(MSG_ERROR, "Altimeter NOT detected!\n");
+		return;
 	}
 
+
+	send16BitWord(0x805D); // soft reset
+	if(!updateOPT()) Telemetry::printf(MSG_ERROR, "Unable to get Altimeter OPT params!\n");
+}
+
+bool Altimeter::updateOPT()
+{
+	// get OPT calibration parameters
+
+	send16BitWord(0xC595); // move register pointer
+	send16BitWordWithCRC(0x0066, 0x9C); // set up OPT Read
+	for(int i=0; i<4; i++)
+	{
+		send16BitWord(0xC7F7); // increment OPT register pointer
+		uint8_t cnbuf[2] = {0};
+		if(readBytes(cnbuf, 2))
+			cn[i] = cnbuf[0] << 8 | cnbuf[1];
+		else
+			return false;
+	}
+	return true;
 }
 
 void Altimeter::getNewSample()
 {
+	if(send16BitWord(ALTIMETER_MODE_NORMAL)) return; // checking for ACK at end of transmission
+	for(int i=0; i<10; i++) {
+		//try to read from altimeter
+		delayMicroseconds(100);
+		uint8_t buf[6] = {0};
+		if(Wire.readBytes(buf, 2))
+		{
+			if(Wire.readBytes(buf+2, 2))
+			{
+				if(Wire.readBytes(buf+4, 2))
+				{
+					uint8_t P_MMSB = buf[0];
+					uint8_t P_MLSB = buf[1];
+					uint8_t P_LMSB = buf[2];
+					uint8_t P_LLSB = buf[3];
+					uint8_t T_MSB = buf[4];
+					uint8_t T_LSB = buf[5];
+					p_dout = P_MMSB << 16 | P_MLSB << 8 | P_LMSB;
+					t_dout = T_MSB << 8 | T_LSB;
+
+					temperature = -45 + 175.0/(float)(1<<16) * t_dout;
+				}
+			}
+		}
+	}
 
 }
 
-void Altimeter::send16BitWord(uint16_t word)
+uint8_t Altimeter::send8BitWord(uint8_t word)
+{
+	Wire.beginTransmission(ALTIMETER_ADDRESS);
+	Wire.send(word & 0xFF);
+	return Wire.endTransmission();
+}
+
+uint8_t Altimeter::send8BitWordWithCRC(uint8_t word, uint8_t crc)
+{
+	Wire.beginTransmission(ALTIMETER_ADDRESS);
+	Wire.send(word & 0xFF);
+	Wire.send(crc);
+	return Wire.endTransmission();
+}
+
+uint8_t Altimeter::send16BitWord(uint16_t word)
 {
 	Wire.beginTransmission(ALTIMETER_ADDRESS);
 	Wire.send((word >> 8) & 0xFF);
 	Wire.send(word & 0xFF);
-	Wire.endTransmission();
+	return Wire.endTransmission();
 }
 
-void Altimeter::readBytes(uint8_t* buf, uint8_t len)
+uint8_t Altimeter::send16BitWordWithCRC(uint16_t word, uint8_t crc)
+{
+	Wire.beginTransmission(ALTIMETER_ADDRESS);
+	Wire.send((word >> 8) & 0xFF);
+	Wire.send(word & 0xFF);
+	Wire.send(crc);
+	return Wire.endTransmission();
+}
+
+bool Altimeter::readBytes(uint8_t* buf, uint8_t len)
 {
 	Wire.requestFrom(ALTIMETER_ADDRESS, 2);
 	if(Wire.available() >= len+1)
@@ -60,8 +130,14 @@ void Altimeter::readBytes(uint8_t* buf, uint8_t len)
 			buf[i] = Wire.read();
 		}
 		uint8_t crc = Wire.read();
-		if(generateCRC8(buf, len) != crc) Telemetry::printf(MSG_ERROR, "Altimeter CRC ERROR!\n");
+		if(generateCRC8(buf, len) != crc)
+		{
+			Telemetry::printf(MSG_ERROR, "Altimeter CRC ERROR!\n");
+			return false;
+		}
+		return true;
 	}
+	return false;
 }
 
 uint8_t Altimeter::generateCRC8(const uint8_t *bytes, uint8_t length)
--- Flight_Electronics_CPU/Altimeter.h
+++ Flight_Electronics_CPU/Altimeter.h
@@ -15,22 +15,35 @@
 #ifndef ALTIMETER_H_
 #define ALTIMETER_H_
 
+
+#define ALTIMITER_MODE_LOW_POWER 0x401A
+#define ALTIMETER_MODE_NORMAL 0x48A3
+#define ALTIMETER_MODE_LOW_NOISE 0x5059
+#define ALTIMETER_MODE_ULTRA_LOW_NOISE 0x58E0
 namespace Altimeter
 {
 	namespace
 	{
+		uint32_t p_dout;
+		uint16_t t_dout;
 		float pressure;
 		float velocity;;
+		float temperature;
+		uint16_t cn[4] = {0};
 		FastCRC8 CRC8;
 	}
 	void init();
 	float getVelocity();
 	float getAlittude();
-	void send16BitWord(uint16_t word);
+	uint8_t send8BitWord(uint8_t word);
+	uint8_t send8BitWordWithCRC(uint8_t word, uint8_t crc);
+	uint8_t send16BitWord(uint16_t word);
+	uint8_t send16BitWordWithCRC(uint16_t word, uint8_t crc);
 	uint8_t generateCRC8(const uint8_t *bytes, uint8_t length);
 	uint8_t generate8bitWordCRC8(uint8_t);
 	uint8_t generate16bitWordCRC8(uint16_t);
-	void readBytes(uint8_t* buf, uint8_t len);
+	bool updateOPT();
+	bool readBytes(uint8_t* buf, uint8_t len);
 	void getNewSample();
 };
 
