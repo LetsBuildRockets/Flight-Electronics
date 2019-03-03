9ff6717113d3d1ac093b9c8b1421e03608c74646
Eric Sims <EricSimsM@gmail.com>
2019-03-02 19:40:21 -0500

    a little shift out code test

--- Flight_Electronics_CPU/libraries/ShiftDigital/ShiftDigital.ino
+++ Flight_Electronics_CPU/libraries/ShiftDigital/ShiftDigital.ino
@@ -2,15 +2,15 @@
 
 void setup() {
  DigitalShift:: init(); // put your setup code here, to run once:
-
+ Serial.begin(9600);
 }
 
 void loop() {
-   DigitalShift::setOutput(6, 1);
-   DigitalShift::setOutput(1, 1);
-   DigitalShift::setOutput(8, 1);
-   DigitalShift::setOutput(10, 1);
-   
+   DigitalShift::setOutput(0, 1);
+   Serial.println("ON");
+   delay (2000);
+   DigitalShift::setOutput(0, 0);
+   Serial.println("OFF");
    delay (2000);
  
 }
